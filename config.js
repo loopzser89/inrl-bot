bc40aa4772452d3e3540355cfbd124e66adc5997
loopzser89 <124852626+loopzser89@users.noreply.github.com>
2023-02-14 15:33:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ module.exports = {
     REACT : process.env.REACT || 'false', //to react  almsg automaticaly
     WARNCOUND : process.env.WARNCOUND || "5", // to kick a person by war cound
     GROUP_CHAT : process.env.GROUP_CHAT || 'false', // to set chat bot on grp
-    FREE_TXT : process.env.FREE_TXT || 'inrlbotmd',//nothing😉
+    FREE_TXT : process.env.FREE_TXT || 'loopzbotmd',//nothing😉
     U_STATUS: process.env.U_STATUS || 'false', //to update profilesatus automaticily
-    ALIVETXT: process.env.ALIVETXT || 'INRL-BOT-MD', //nothing😛
+    ALIVETXT: process.env.ALIVETXT || 'LOOPZ-BOT-MD', //nothing😛
     ALIVE_DATA : process.env.ALIVE_DATA || "https://i.imgur.com/DyLAuEh.jpg,aliveText,alivebButtenText1,aliveTextButten2",// coustamized alive bottun😛
     READ_CHAT : process.env.READ_CHAT || "false", //values are true and false only if true read all chats
-    BOT_INFO : process.env.BOT_INFO || "INRL-BOT-MD,INRL,https://i.imgur.com/DyLAuEh.jpg", // about the bot as img number for menu such plugins
+    BOT_INFO : process.env.BOT_INFO || "LOOPZ-BOT-MD,INRL,https://i.imgur.com/DyLAuEh.jpg", // about the bot as img number for menu such plugins
     BGMBOT : process.env.BGMBOT || 'false', //to set bot as bgm bot
     WORKTYPE: process.env.WORKTYPE || 'privet', //bot working mode
     PM_BLOCK : process.env.PM_BLOCK || "false", // to block all pm msg expt creaters as sudo
@@ -29,7 +29,7 @@ module.exports = {
         MENSION_TEXT : process.env.MENSION_TEXT || 'inrl-md,edit this and set your data, https://chat.whatsapp.com/CUJuk1xPEpR8av9h7DDGNO'
     },
     LANG : (process.env.LANGUAG || 'EN').toLowerCase(), //values are ml, en only
-    OWNER : process.env.OWNER || "917593919575",// bot owner
+    OWNER : process.env.OWNER || "919037858501",// bot owner
     BRANCH: 'master',//nothing😉
     PROFILE_STATUS : process.env.PROFILE_STATUS || "💗 ᴀᴜᴛᴏ ᴍᴀᴛɪᴄ ʙɪᴏ ʙy ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ",// profile status updating value
     HEROKU: {
@@ -52,7 +52,7 @@ module.exports = {
     WEB : process.env.WEB || "https://tinyurl.com/ycks3s8p",//your website url
     YT : process.env.YT || "https://tinyurl.com/36r3668n",//your yt url
     CAPTION : process.env.CAPTION || "_created by inrl-bot_",//captions of img and videos
-    SUDO: process.env.SUDO || "917593919575",// sudo numbers to manage bot it will been add more by,
+    SUDO: process.env.SUDO || "919037858501",// sudo numbers to manage bot it will been add more by,
     VIDEO : "https://tinyurl.com/3x38ajmn",//turtorial video to watch how to use bot
     WAGRP : process.env.WAGRP || 'https://tinyurl.com/f5wh55mk',//your wa group url
     };
